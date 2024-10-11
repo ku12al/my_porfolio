14804ca41cfb7ad3b48600e9a77d11ed4f40a59c
kunal <ks1140175@gmail.com>
2024-10-11 14:35:53 +0530

    last commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,9 +26,9 @@ const Hero = () => {
       </div>
       <ComputersCanvas />
 
-      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+      <div className="absolute bottom-3 xs:bottom-0 w-full flex justify-center items-center">
         <a href="#about">
-          <div className="w-[30px] h-[55px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+          <div className="w-[30px] h-[55px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2 ">
             <motion.dev
               animate={{ y: [0, 24, 0] }}
               transition={{
