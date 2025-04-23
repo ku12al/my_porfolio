170cb92e2551f9b0823babac6883affc8fe29ad2
kunal <ks1140175@gmail.com>
2025-04-23 11:42:24 +0530

    updated code

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -54,7 +54,7 @@ const Hero = () => {
       </div>
       {/* <ComputersCanvas /> */}
 
-      <div className="absolute bottom-3 xs:bottom-1 w-full flex justify-center items-center mt-10">
+      <div className="absolute bottom-3 xs:bottom-1 w-full flex justify-center items-center mt-10 xs:mt-15">
         <a href="#about">
           <div className="w-[30px] h-[55px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
             <motion.dev
